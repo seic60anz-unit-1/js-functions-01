1d8880780596b1a9d05ad72848fca6160626af81
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-07 14:28:57 +1100

    Which Number is Bigger?

--- script.js
+++ script.js
@@ -8,8 +8,21 @@
 
 //How Much Land
 
-const howMuchLand = function(length, width) {
-    const area = length * width;
-    return `You have this many square metres of land: ${area}`
+// const howMuchLand = function(length, width) {
+//     const area = length * width;
+//     return `You have this many square metres of land: ${area}`
+// }
+// console.log(howMuchLand(30, 20))
+
+// Which Number is Bigger?
+
+const greaterNum = function(x, y) {
+    if (x >= y) {
+        return `The greater number between ${x} and ${y} is ${x}.`
+    } else {
+        return `The greater number between ${x} and ${y} is ${y}.`
+    }
 }
-console.log(howMuchLand(30, 20))
+
+console.log(greaterNum(8,11))
+console.log(greaterNum(13,10))
