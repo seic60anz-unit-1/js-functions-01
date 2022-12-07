cee1648028c41ed70232dd1d9cb0dcb920e41108
Jeffrey Fazal <jeffreyfazal@gmail.com>
2022-12-07 14:57:54 +1100

    Completed

--- script.js
+++ script.js
@@ -28,3 +28,23 @@ const num1 = 10
 const num2 = 5
 outcome = greaterNum(num1,num2)
 console.log(`The greater number between ${num1} and ${num2} is ${outcome}`)
+
+// Grader Assigner
+
+const assginGrade = function (grade) {
+    if (grade > 85) {
+        console.log("A") // Can return, return a string (e.g. return 'A')
+    } else if (grade > 75) {
+        console.log("B")
+    } else if (grade > 65) {
+        console.log("C")
+    }else if (grade > 55) {
+        console.log("D")
+    } else if (grade < 50) {
+        console.log("F")
+    }
+    }
+
+assginGrade(88)
+assginGrade(40)
+assginGrade(74)
