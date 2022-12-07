50de3b4054b274ebb72357cd4a882a76eaac90e0
JP <jpcodingjourney@JPs-MacBook-Air.local>
2022-12-07 14:52:45 +1100

    Intro to Functions

---
+++ script.js
@@ -0,0 +1,52 @@
+
+// How Much Land
+// const greetings = function (name) {
+//     console.log('Nice to meet you ' + name + " !")
+// }
+// greetings('JJ')
+
+// Which Number is Bigger?
+// const howMuchLand = function (length, width) {
+//     return(`You have this many square metres of land: ${length * width}`)
+// }
+// console.log(howMuchLand(250, 300))
+
+// Which Number is Bigger?
+
+// const greaterNum = function (num1, num2) {
+//     if (num1 > num2) {
+//         return(`The greater number between ${num1} and ${num2} is ${num1}.`)
+//     } else if (num1 < num2) {
+//         return(`The greater number between ${num1} and ${num2} is ${num2}.`)
+//     } else {
+//         return('The two numbers are equal!')
+//     }
+// } 
+// console.log(greaterNum(250, 350))
+// console.log(greaterNum(4, 2))
+// console.log(greaterNum(10, 10))
+
+
+// const assignGrade = function(score) {
+//     if (score >=85 && score <= 100) {
+//         return("A")
+//     } else if (score >= 75 && score < 85) {
+//         return("B")
+//     } else if (score >= 65 && score < 75) {
+//         return("C")
+//     } else if (score >= 50 && score < 65) {
+//         return("D")
+//     } else if (score < 50 && score >= 0) {
+//         return("E")
+//     } else {
+//         return("Your score is not from this world!")
+//     }
+// }
+
+// console.log(assignGrade(100))
+// console.log(assignGrade(85))
+// console.log(assignGrade(81.5))
+// console.log(assignGrade(66))
+// console.log(assignGrade(50))
+// console.log(assignGrade(420))
+// console.log(assignGrade(-243))
