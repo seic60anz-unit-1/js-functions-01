350895ad71918f08c1fc45a3ddc3bb40ea7b6f59
Penelope Chen <pen.c828@gmail.com>
2022-12-07 14:38:20 +1100

    1st exercise done

---
+++ script.js
@@ -0,0 +1,15 @@
+// The Introduction 
+const greetings = function (name) {
+    console.log(`Nice to meet you ${name}!`)
+} 
+
+greetings("Skitter")
+greetings("Tattletale")
+
+// How Much Land 
+const howMuchLand = function (length, width) {
+    const area = length * width 
+    return area 
+}
+
+console.log()
